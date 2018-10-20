18c173cedc323025448298ce3b5124fc7f8b77be
Mobious <sammiel.gross@gmail.com>
2018-10-20 12:08:24 -0500

    unpdate

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
 console.log("this is loaded");
 
+require("dotenv").config();
+
 exports.gmaps = {
   api: process.env.Google_maps_key
 };
--- public/js/maps.js
+++ public/js/maps.js
@@ -1,16 +1,14 @@
-require("dotenv").config();
-
-
-const googleMapsClient = require('@google/maps').createClient({
-    key: dataKeys.gmaps,
-    Promise: Promise
+const googleMapsClient = require("@google/maps").createClient({
+  key: dataKeys.gmaps,
+  Promise: Promise
+});
+googleMapsClient
+  .geocode({ address: "1600 Amphitheatre Parkway, Mountain View, CA" })
+  .asPromise()
+  .then(function(response) {
+    console.log(response.json.results);
+  })
+  .catch(function(err) {
+    console.log(err);
   });
-   
-  googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'})
-    .asPromise()
-    .then((response) => {
-      console.log(response.json.results);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+createClient();
