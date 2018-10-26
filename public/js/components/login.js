8f2317f8748d1fd19eed9c5d7662946b62577d64
jordan <jgcoon24@gmail.com>
2018-10-26 18:30:06 -0500

    stupid modal

--- public/js/components/login.js
+++ public/js/components/login.js
@@ -1,96 +1,87 @@
-const React = require('react');
-const auth = require('../auth');
+// login modal ////
+$(function() {
+    
+  var $formLogin = $('#login-form');
+  var $formLost = $('#lost-form');
+  var $formRegister = $('#register-form');
+  var $divForms = $('#div-forms');
+  var $modalAnimateTime = 300;
+  var $msgAnimateTime = 150;
+  var $msgShowTime = 2000;
 
-const Login = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.object.isRequired
-  },
-  getInitialState : function() {
-    return {
-      error: false
-    }
-  },
-  componentDidMount: function() {
-    $('#myLogin').modal('show')
-  },
-
-  handleSubmit : function(event) {
-    event.preventDefault()
-
-    const email = this.refs.email.value
-    const password = this.refs.password.value
-
-    auth.login(email, password, (loggedIn) => {
-      if (!loggedIn)
-        return this.setState({ error: true })
-      const { location } = this.props
-
-      if (location.state && location.state.nextPathname) {
-        this.context.router.replace(location.state.nextPathname)
-      } else {
-        var user_id = {
-        user_id: parseInt(localStorage.user_id)
-        }
-        $.post('/toto/private', user_id)
-          .done((data)=>{
-            localStorage.myToto = JSON.stringify(data)
-          })
-          .error((error)=>{
-            console.log('Error getting privat toto!', error);
-          })
-        this.context.router.replace('/')
+  $("form").submit(function () {
+      switch(this.id) {
+          case "login-form":
+              var $lg_username=$('#login_username').val();
+              var $lg_password=$('#login_password').val();
+              if ($lg_username == "ERROR") {
+                  msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Login error");
+              } else {
+                  msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Login OK");
+              }
+              return false;
+              break;
+          case "lost-form":
+              var $ls_email=$('#lost_email').val();
+              if ($ls_email == "ERROR") {
+                  msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "Send error");
+              } else {
+                  msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "success", "glyphicon-ok", "Send OK");
+              }
+              return false;
+              break;
+          case "register-form":
+              var $rg_username=$('#register_username').val();
+              var $rg_email=$('#register_email').val();
+              var $rg_password=$('#register_password').val();
+              if ($rg_username == "ERROR") {
+                  msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
+              } else {
+                  msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "success", "glyphicon-ok", "Register OK");
+              }
+              return false;
+              break;
+          default:
+              return false;
       }
-    })
-    $('#myLogin').modal('hide')
-  },
-  render : function() {
-    return (
-      <div id="myLogin" role="dialog" className="modal fade">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal">×</button>
-              <h4><span className="glyphicon glyphicon-heart" /> Login</h4>
-            </div>
-
-            <div className="modal-body" id="loginform">
-              <form role="form" onSubmit={this.handleSubmit}>
-
-                <div className="input-group input-group-md col-md-6 col-md-offset-3" style={{marginBottom: 10}}>
-                  <span className="input-group-addon">
-                    <span className="glyphicon glyphicon-envelope"></span>
-                  </span>
-                  <input ref="email" type="email" id="inputEmail" className="form-control"  placeholder="Email address" autofocus />
-                </div>
-
-                <div className="input-group input-group-md col-md-6 col-md-offset-3" style={{marginBottom: 10}}>
-                  <span className="input-group-addon">
-                    <span className="glyphicon glyphicon-lock"></span>
-                  </span>
-                  <input ref="password" type="password" id="inputPassword" className="form-control" placeholder="Password" />
-                </div>
-
-                <button type="submit" className="btn" style={{marginTop: 10}}>Login
-                  <span className="glyphicon glyphicon-ok" />
-                </button>
-
-              </form>
-            </div>
-
-            <div className="modal-footer">
-              <button type="submit" className="btn btn-danger btn-default pull-left" data-dismiss="modal">
-                <span className="glyphicon glyphicon-remove" /> Cancel
-              </button>
-              <p>Need <a href="#">help?</a></p>
-            </div>
-            {this.state.error && (
-            <p>Password and email do not match</p>
-            )}
-          </div>
-        </div>
-      </div>
-    )
+      return false;
+  });
+  
+  $('#login_register_btn').click( function () { modalAnimate($formLogin, $formRegister) });
+  $('#register_login_btn').click( function () { modalAnimate($formRegister, $formLogin); });
+  $('#login_lost_btn').click( function () { modalAnimate($formLogin, $formLost); });
+  $('#lost_login_btn').click( function () { modalAnimate($formLost, $formLogin); });
+  $('#lost_register_btn').click( function () { modalAnimate($formLost, $formRegister); });
+  $('#register_lost_btn').click( function () { modalAnimate($formRegister, $formLost); });
+  
+  function modalAnimate ($oldForm, $newForm) {
+      var $oldH = $oldForm.height();
+      var $newH = $newForm.height();
+      $divForms.css("height",$oldH);
+      $oldForm.fadeToggle($modalAnimateTime, function(){
+          $divForms.animate({height: $newH}, $modalAnimateTime, function(){
+              $newForm.fadeToggle($modalAnimateTime);
+          });
+      });
   }
-})
-
-module.exports = Login;
+  
+  function msgFade ($msgId, $msgText) {
+      $msgId.fadeOut($msgAnimateTime, function() {
+          $(this).text($msgText).fadeIn($msgAnimateTime);
+      });
+  }
+  
+  function msgChange($divTag, $iconTag, $textTag, $divClass, $iconClass, $msgText) {
+      var $msgOld = $divTag.text();
+      msgFade($textTag, $msgText);
+      $divTag.addClass($divClass);
+      $iconTag.removeClass("glyphicon-chevron-right");
+      $iconTag.addClass($iconClass + " " + $divClass);
+      setTimeout(function() {
+          msgFade($textTag, $msgOld);
+          $divTag.removeClass($divClass);
+          $iconTag.addClass("glyphicon-chevron-right");
+          $iconTag.removeClass($iconClass + " " + $divClass);
+    }, $msgShowTime);
+  }
+});
