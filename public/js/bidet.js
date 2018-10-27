9341c60ba92083df1b8c4e29e5e985dd276973ff
Kane McGree <kmcgree14@gmail.com>
2018-10-27 08:33:22 -0500

    lint fixes

--- public/js/bidet.js
+++ public/js/bidet.js
@@ -1,23 +1,31 @@
 $(document).ready(() => {
-  $(".addBidetFormm").on("submit", function (event) {
+  $(".addBidetFormm").on("submit", function(event) {
     event.preventDefault();
-    const buildingName = $(this).children('#buildingName').val();
-    const address = $(this).children('#address').val();
-    const toiletType = $(this).children('#toiletType').val();
-    const extraDetails = $(this).children('#extraDetails').val();
-    const img = $(this).children('#img').val();
-
+    const buildingName = $(this)
+      .children("#buildingName")
+      .val();
+    const address = $(this)
+      .children("#address")
+      .val();
+    const toiletType = $(this)
+      .children("#toiletType")
+      .val();
+    const extraDetails = $(this)
+      .children("#extraDetails")
+      .val();
+    const img = $(this)
+      .children("#img")
+      .val();
 
     const bidet = {
       buildingName,
       address,
       toiletType,
       extraDetails,
-      img 
+      img
     };
     $.post("/api/bidet/create", bidet, () => {
-      window.location.href = "/"
+      window.location.href = "/";
     });
   });
-})
-
+});
