e2deeb8918094ca098242a326139b402a3c9dffd
Sam Gross <sammiel.gross@gmail.com>
2018-12-17 11:15:13 -0600

    nodemon

--- models/bidet.js
+++ models/bidet.js
@@ -1,29 +1,29 @@
-module.exports = (sequelize, DataTypes) => {
-  const Bidet = sequelize.define("Bidet", {
-    buildingName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    toiletType: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    extraDetails: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      defaultValue: null,
-      unique: true
-    },
-    img: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    }
-  });
-  return Bidet;
-};
+// module.exports = (sequelize, DataTypes) => {
+//   const Bidet = sequelize.define("Bidet", {
+//     buildingName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       unique: true
+//     },
+//     address: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       unique: true
+//     },
+//     toiletType: {
+//       type: DataTypes.STRING,
+//       allowNull: true
+//     },
+//     extraDetails: {
+//       type: DataTypes.STRING,
+//       allowNull: true,
+//       defaultValue: null,
+//       unique: true
+//     },
+//     img: {
+//       type: DataTypes.TEXT,
+//       allowNull: false
+//     }
+//   });
+//   return Bidet;
+// };
